c866dfcc25266873bede54b8d225409c3461ca7f
oliver <oliverlcruickshank@gmail.com>
2019-02-08 17:03:46 +0000

    Readded folders for .gitignore

---
+++ compiler/picoblaze_compiler.h
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum KeywordType: int{
+    KEYWORD_JUMP = 0,
+    KEYWORD_CALL,
+    KEYWORD_RETURN,
+    KEYWORD_ADD,
+    KEYWORD_ADDC,
+    KEYWORD_SUB,
+    KEYWORD_SUBC,
+    KEYWORD_COMPARE,
+    KEYWORD_LOAD,
+    KEYWORD_AND,
+    KEYWORD_OR,
+    KEYWORD_XOR,
+    KEYWORD_SR0,
+    KEYWORD_SR1,
+    KEYWORD_SRX,
+    KEYWORD_SRA,
+    KEYWORD_RR,
+    KEYWORD_SL0,
+    KEYWORD_SL1,
+    KEYWORD_SLX,
+    KEYWORD_SLA,
+    KEYWORD_RL,
+    KEYWORD_STORE,
+    KEYWORD_FETCH,
+    KEYWORD_INPUT,
+    KEYWORD_OUTPUT,
+    
+    KEYWORD_Z,
+    KEYWORD_NZ,
+    KEYWORD_C,
+    KEYWORD_NC,
+    
+    KEYWORD_LABEL,
+    
+    KEYWORD_INVALID
+    
+};
+
+enum TokenType: int{
+    TOKEN_INVALID,
+    
+    TOKEN_FULLSTOP,
+    TOKEN_SEMICOLON,
+    TOKEN_COLON,
+    TOKEN_COMMA,
+    TOKEN_CONDITION,
+    TOKEN_INSTRUCTION,
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER_LITERAL,
+    TOKEN_OPEN_BRACE,
+    TOKEN_CLOSE_BRACE,
+    
+    TOKEN_END,
+    
+};
+
+
+enum Opcode: unsigned int{
+    OPCODE_JUMP_U = 0x22000,
+    OPCODE_JUMP_Z = 0x32000,
+    OPCODE_JUMP_NZ = 0x36000,
+    OPCODE_JUMP_C = 0x3A000,
+    OPCODE_JUMP_NC = 0x3E000,
+    
+    OPCODE_CALL_U = 0x20000,
+    OPCODE_CALL_Z = 0x30000,
+    OPCODE_CALL_NZ = 0x34000,
+    OPCODE_CALL_C = 0x38000,
+    OPCODE_CALL_NC = 0x3C000,
+    
+    OPCODE_RETURN_U = 0x25000,
+    OPCODE_RETURN_Z = 0x31000,
+    OPCODE_RETURN_NZ = 0x35000,
+    OPCODE_RETURN_C = 0x39000,
+    OPCODE_RETURN_NC = 0x3D000,
+    
+    
+    OPCODE_ADD_REGISTER = 0x10000,
+    OPCODE_ADD_CONSTANT = 0x11000,
+    
+    OPCODE_ADDC_REGISTER = 0x12000,
+    OPCODE_ADDC_CONSTANT = 0x13000,
+    
+    OPCODE_SUB_REGISTER = 0x18000,
+    OPCODE_SUB_CONSTANT = 0x19000,
+    
+    OPCODE_SUBC_REGISTER = 0x1A000,
+    OPCODE_SUBC_CONSTANT = 0x1B000,
+    
+    OPCODE_COMPARE_REGISTER = 0x1C000,
+    OPCODE_COMPARE_CONSTANT = 0x1D000,
+    
+    OPCODE_COMPAREC_REGISTER = 0x1E000,
+    OPCODE_COMPAREC_CONSTANT = 0x1F000,
+    
+    OPCODE_LOAD_REGISTER = 0x00000,
+    OPCODE_LOAD_CONSTANT= 0x01000,
+    
+    OPCODE_AND_REGISTER = 0x02000,
+    OPCODE_AND_CONSTANT = 0x03000,
+    
+    OPCODE_OR_REGISTER = 0x04000,
+    OPCODE_OR_CONSTANT = 0x05000,
+    
+    OPCODE_XOR_REGISTER = 0x06000,
+    OPCODE_XOR_CONSTANT = 0x07000,
+    
+    OPCODE_SL0 = 0x14006,
+    OPCODE_SL1 = 0x14007,
+    OPCODE_SLX = 0x14004,
+    OPCODE_SLA = 0x14000,
+    OPCODE_RL = 0x14002,
+    OPCODE_SR0 = 0x1400E,
+    OPCODE_SR1 = 0x1400F,
+    OPCODE_SRX = 0x1400A,
+    OPCODE_SRA = 0x14008,
+    OPCODE_RR = 0x1400C,
+    
+    OPCODE_STORE_REGISTER = 0x2E000,
+    OPCODE_STORE_CONSTANT = 0x2F000,
+    
+    OPCODE_FETCH_REGISTER = 0x0A000,
+    OPCODE_FETCH_CONSTANT = 0x0B000,
+    
+    OPCODE_INPUT_REGISTER = 0x08000,
+    OPCODE_OUTPUT_REGISTER = 0x2C000,
+    
+    OPCODE_HALT= 0xFF000
+};
+
+struct String{
+    char* text;
+    int len;
+};
+
+
+struct Token{
+    String str;
+    TokenType type;
+};
+
+struct Hash_Node{
+    Hash_Node* next = nullptr;
+    int value;
+};
+#define HASH_SIZE 211
+
+struct Map{
+    Hash_Node* nodes[HASH_SIZE];
+    int size;
+};
+
+struct Lexer{
+    char* pos;
+    bool error;
+    int instruction_count = 0;
+    Map label_map;
+};
+
+static char* instructions[]{
+    "jump","call", "return", "add", "addc", "sub", "subc", "compare",
+    "load", "and", "or", "xor", "sr0", "sr1", "srx", "sra", "rr",
+    "sl0", "sl1", "slx", "sla", "rl", "store", "fetch", "input", "output",
+};
+
+static char* conditions[]{
+    "nz", "n", "nc", "c"
+};
+
+
+#define array_count(x) sizeof(x)/sizeof(x[0])
---
+++ vm/picoblaze_vm.cc
@@ -0,0 +1,150 @@
+#include "picoblaze_vm.h"
+
+
+static void
+open_object_file(char* filename,unsigned int* mem){
+    
+    FILE* file_pointer = fopen(filename, "rb");
+    
+    if(file_pointer){
+        fseek(file_pointer, 0, SEEK_END);
+        size_t size = ftell(file_pointer);
+        fseek(file_pointer, 0, SEEK_SET);
+        
+        fread(mem, size, 1, file_pointer);
+        fclose(file_pointer);
+    }else {
+        printf("Unable to open file");
+    }
+}
+
+#define get_opcode(x) ((x) & 0x000FF000)
+#define get_regx(x) (((x) & 0x00000F00) >> 8)
+#define get_regy(x) (((x) & 0x000000F0) >> 4)
+#define get_address(x) ((x) & 0x00000FFF)
+#define get_constant(x) ((x) & 0x000000FF)
+
+#define STACKSIZE 16
+#define RAMSIZE 256
+#define MEMSIZE 64
+
+static void
+display_memory(unsigned char* stack, unsigned char* ram, unsigned int* mem){
+    for(int i = 0; i < STACKSIZE; i++){
+        printf("%x ", stack[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < RAMSIZE; i+=8){
+        for(int j = 0; j < 8; j++){
+            printf("%x ", ram[i+j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char** args){
+    
+    unsigned char stack[STACKSIZE] = {};
+    unsigned char ram[RAMSIZE] = {};
+    unsigned int mem[MEMSIZE] = {};
+    
+    unsigned char pc = 0;
+    Status status = {};
+    
+    open_object_file(args[1], mem);
+    
+    bool running = true;
+    
+    while(running){
+        unsigned int instruction = mem[pc];
+        unsigned int opcode = get_opcode(instruction);
+        switch(opcode){
+            case OPCODE_LOAD_CONSTANT:{
+                int regx = get_regx(instruction);
+                int constant = get_constant(instruction);
+                stack[regx] = constant;
+            }break;
+            case OPCODE_LOAD_REGISTER:{
+                int regx = get_regx(instruction);
+                int regy = get_regy(instruction);
+                stack[regx] = stack[regy];
+            }break;
+            case OPCODE_JUMP_U:{
+                int address = get_address(instruction);
+                pc = --address;
+            }break;
+            case OPCODE_JUMP_NZ:{
+                int address = get_address(instruction);
+                if(!status.zero){
+                    pc = --address;
+                }
+            }break;
+            case OPCODE_JUMP_Z:{
+                int address = get_address(instruction);
+                if(status.zero){
+                    pc = --address;
+                }
+            }break;
+            case OPCODE_JUMP_NC:{
+                int address = get_address(instruction);
+                if(!status.carry){
+                    pc = --address;
+                }
+            }break;
+            case OPCODE_ADD_CONSTANT:{
+                int regx = get_regx(instruction);
+                int constant = get_constant(instruction);
+                unsigned value = stack[regx] + constant;
+                if(value < stack[regx]) {
+                    status.carry = true;
+                }else{
+                    status.carry = true;
+                }
+                stack[regx] = value;
+                
+            }break;
+            case OPCODE_SUB_CONSTANT:{
+                int regx = get_regx(instruction);
+                int constant = get_constant(instruction);
+                stack[regx] -= constant;
+                if(stack[regx] == 0) {
+                    status.zero = true;
+                }else{
+                    status.zero = false;
+                }
+            }break;
+            case OPCODE_SUB_REGISTER:{
+                int regx = get_regx(instruction);
+                int regy = get_regy(instruction);
+                if((int16_t)stack[regx] - (int16_t)stack[regy] < 0){
+                    status.carry = true;
+                }else{
+                    status.carry = false;
+                }
+                stack[regx] -= stack[regy];
+                if(stack[regx] == 0) {
+                    status.zero = true;
+                }else{
+                    status.zero = false;
+                }
+            }break;
+            case OPCODE_INPUT_REGISTER:{
+                int regx = get_regx(instruction);
+                int regy = get_regy(instruction);
+                stack[regx] = ram[stack[regy]];
+            }break;
+            case OPCODE_OUTPUT_REGISTER:{
+                int regx = get_regx(instruction);
+                int regy = get_regy(instruction);
+                ram[stack[regy]] = stack[regx];
+            }break;
+            case OPCODE_HALT:{
+                running = false;
+            }break;
+        }
+        printf("%d\n", pc);
+        pc++;
+    }
+    display_memory(stack, ram, mem);
+    return 0;
+}
